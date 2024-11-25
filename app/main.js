83544a372d783a10f90deb4e249cfbab0d099e89
Athena <athenachang17@gmail.com>
2024-11-25 12:00:00 -0500

    Notes with Ben

---
+++ app/main.js
@@ -0,0 +1,15 @@
+import './style.css'
+import data from './data.json';
+import { v4 as generateUUID } from 'uuid';
+console.log(data);
+console.log(generateUUID());
+
+/* 
+1. delete everything in main
+2. delete everything in style
+3. delete the counter.js file
+4. delete the javascript.svg file
+5. delete the public/vite.svg file
+
+npm i uuid (uuid = universal unique identifier)
+*/
